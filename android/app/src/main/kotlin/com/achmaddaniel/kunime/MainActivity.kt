c733817cff1162c03dd884ba3e53b9e631a977a3
kudanilll <achmad24daniel@gmail.com>
2024-12-20 02:41:13 +0700

    fix merge

--- android/app/src/main/kotlin/com/achmaddaniel/kunime/MainActivity.kt
+++ android/app/src/main/kotlin/com/achmaddaniel/kunime/MainActivity.kt
@@ -2,8 +2,4 @@ package com.achmaddaniel.kunime
 
 import io.flutter.embedding.android.FlutterActivity
 
-<<<<<<< HEAD
 class MainActivity: FlutterActivity()
-=======
-class MainActivity: FlutterActivity()
->>>>>>> 0a9065c99c02dc8b063f4a742cc31243c113bc56
