9efb9a83eb09f15d0a388d2f9e0fa8d68dd2c37d
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2025-11-12 16:56:53 +0700

    style(widgets): fix trailing commas and padding in trending anime list
    - Add trailing commas to all map entries for consistent formatting
    - Simplify padding declaration using fromLTRB
    - Remove redundant style property grouping

--- lib/widgets/listview/trending_anime_list.dart
+++ lib/widgets/listview/trending_anime_list.dart
@@ -5,27 +5,27 @@ final List<Map<String, dynamic>> trendingAnimes = [
   {
     "imageUrl": "https://cdn.myanimelist.net/images/anime/1000/110531.jpg",
     "title": "Attack on Titan",
-    "episode": 75
+    "episode": 75,
   },
   {
     "imageUrl": "https://cdn.myanimelist.net/images/anime/1286/99889.jpg",
     "title": "Demon Slayer",
-    "episode": 26
+    "episode": 26,
   },
   {
     "imageUrl": "https://cdn.myanimelist.net/images/anime/1911/113611.jpg",
     "title": "My Hero Academia",
-    "episode": 113
+    "episode": 113,
   },
   {
     "imageUrl": "https://cdn.myanimelist.net/images/anime/1244/138851.jpg",
     "title": "One Piece",
-    "episode": 1000
+    "episode": 1000,
   },
   {
     "imageUrl": "https://cdn.myanimelist.net/images/anime/1565/111305.jpg",
     "title": "Naruto Shippuden",
-    "episode": 24
+    "episode": 24,
   },
 ];
 
@@ -38,13 +38,10 @@ class TrendingAnimeList extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          padding: EdgeInsets.fromLTRB(16, 0, 16, 12),
           child: Text(
             'Trending Minggu Ini',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
         ListView.builder(
