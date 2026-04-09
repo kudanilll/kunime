e3d6eb145801ca62c8506ef754924965f4501b06
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2026-04-09 22:55:27 +0700

    refactor(genre_card): remove unused import of flutter/services (#13)
    The Flutter Services import was not being used in the file, so it has been removed to clean up the code and improve maintainability.
    
    Signed-off-by: Achmad Daniel Syahputra <achmad24daniel@gmail.com>

--- lib/features/home/presentation/sections/genre/widgets/genre_card.dart
+++ lib/features/home/presentation/sections/genre/widgets/genre_card.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:kunime/core/themes/app_colors.dart';
 import 'package:kunime/features/home/models/genre/ui_genre.dart';
 
